7e40f58a045d802b52c6c37a62b8fc85608505e6
“juliantreweeke” <juliantreweeke@gmail.com>
2017-07-10 15:52:37 +1000

    ade search partial and fixed new post

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get '/posts/map' => 'posts#map'
 
   post '/posts/search' => 'posts#do_search'
+  # post '/posts/search' => 'posts#do_search'
 
   resources :comments
   resources :users
