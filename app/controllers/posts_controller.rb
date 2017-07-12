8642befd062b3f45be4441dd85eeafa66f0dc161
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-07-12 10:14:37 +1000

    added more finctionality to non users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :check_if_admin, only: [:edit, :destroy]
-  before_action :check_if_logged_in, except: [:map, :do_search]
+  before_action :check_if_logged_in, except: [:map, :do_search, :show]
 
   # GET /posts
   # GET /posts.json
