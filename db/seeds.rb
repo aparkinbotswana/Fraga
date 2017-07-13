a9f8d49a15e86af4cf8476ea9a524b40306f9306
Michelle Erica <michelle.erica.lo@gmail.com>
2017-07-13 19:16:31 +1000

    in the midst of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,12 @@ u1 = User.create username: "Boolean", latitude: "-33.868820", longitude: "151.20
 u2 = User.create username: "Mad Slingshot", latitude: "-33.869795", longitude: "151.206410", password: "chicken", is_admin: false
 u3 = User.create username: "Ruler Monarch", latitude: "-33.873157", longitude: "151.206116", password: "chicken", is_admin: false
 u4 = User.create username: "chicken",latitude: "-33.873157", longitude: "151.206116", password: "chicken", is_admin: true
-u5 = User.create username: "Robbo",latitude: "54.664097", longitude: "-2.752708", password: "chicken", is_admin: true
+u5 = User.create username: "Rudolph 'Poker Face' Holmes", latitude: "40.7143528", longitude: "-74.0059731", password: "chicken", is_admin: false
+u6 = User.create username: "Clinton 'Wager' Hendricks", latitude: "40.71916023", longitude: "-73.93180847", password: "chicken", is_admin: false
+u7 = User.create username: "Ray 'Hazard' Bush", latitude: "40.58788529", longitude: "-80.08415222", password: "chicken", is_admin: false
+u8 = User.create username: "Reuben 'the Demon' McCall", latitude: "40.58788529", longitude: "-80.08415225", password: "chicken", is_admin: false
+u9 = User.create username: "Fred 'Smokes' Carr", latitude: "34.7777158", longitude: "-118.89404297", password: "chicken", is_admin: false
+u10 = User.create username: "Anton 'Ambush' Durham", latitude: "35.42486792", longitude: "137.1862793", password: "chicken", is_admin: false
 
 puts "Created #{ User.all.length } users."
 
@@ -21,7 +26,15 @@ p2 = Post.create question: "Where can I buy stuff?", latitude: "-33.891475", lon
 p3 = Post.create question: "Any recommendations for cool shit?", latitude: "-33.893340", longitude: "151.204610", active: true, emjoi: "happy"
 p4 = Post.create question: "Kyrka-nyckel varg destillery direkt handel ironi estetiska banjo varje dag bär varm kyckling ", latitude: "-33.898340", longitude: "151.204250", active: true, emjoi: "sad"
 p5 = Post.create question: "Where is the best food around here bruh? School me bruzzy", latitude: "54.664097", longitude: "-2.752708", active: true, emjoi: "bored"
-p5 = Post.create question: "Any cheap food around these parts bro? Hungover as...", latitude: "-33.771000", longitude: "150.906300", active: true, emjoi: "devo"
+p6 = Post.create question: "I lost my camera, has anyone found it?", latitude: "34.7777158", longitude: "-118.89404297", active: true, emjoi: "devo"
+p7 = Post.create question: "Where can I get a drink?", latitude: "40.71916023", longitude: "-73.93180847", active: true, emjoi: "wasted"
+p8 = Post.create question: "When do shops close around here?", latitude: "40.7143528", longitude: "-74.0059731", active: true, emjoi: "devo"
+p9 = Post.create question: "What can I do around here on weekends?", latitude: "-33.771500", longitude: "150.906600", active: true, emjoi: "devo"
+p10 = Post.create question: "Where can I learn Welsh?", latitude: "45.05800144", longitude: "34.10705566", active: true, emjoi: "thinking"
+p11 = Post.create question: "Avez-vous des animaux domestiques?", latitude: "‎48.864716", longitude: "2.349014", active: true, emjoi: "cheeky"
+p12 = Post.create question: "La Tour Eiffel est-elle bonne?", latitude: "‎48.864720", longitude: "2.349024", active: true, emjoi: "happy"
+p13 = Post.create question: "Quel est le meilleur, étant le patron ou un employé?", latitude: "‎48.864720", longitude: "2.349024", active: true, emjoi: "confused"
+p14 = Post.create question: "Où puis-je obtenir un plat maison?", latitude: "‎48.8647215", longitude: "2.349027", active: true, emjoi: "silly"
 
 puts "Created #{ Post.all.length } users."
 
